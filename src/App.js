21eb73b26a911282e601b7814ab7232ef58259d1
montibel <113550254+montibel@users.noreply.github.com>
2022-11-16 15:33:31 +0100

    it1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
 import './App.css';
 import foods from './foods.json';
+import React, { useState } from 'react';
 
 function App() {
+  const [allFood, setAllFood] = useState(foods);
   return (
     <div className="App">
-      
+      {allFood.map((food) => {
+        return (
+          <div>
+            <p> {food.name} </p>
+            <img src={food.image} style={{ width: '100px', height: '100px' }} />
+          </div>
+        );
+      })}
     </div>
   );
 }
