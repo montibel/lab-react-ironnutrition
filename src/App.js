07513461dc326c5f3fc02a23119bf03736c80f3a
montibel <113550254+montibel@users.noreply.github.com>
2022-11-16 16:04:11 +0100

    done, I like React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import foods from './foods.json';
 import React, { useState } from 'react';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import FoodBox from './components/FoodBox';
 
 function App() {
   const [allFood, setAllFood] = useState(foods);
@@ -9,8 +11,17 @@ function App() {
       {allFood.map((food) => {
         return (
           <div>
-            <p> {food.name} </p>
             <img src={food.image} style={{ width: '100px', height: '100px' }} />
+            <p> {food.name} </p>
+
+            <FoodBox
+              food={{
+                name: 'Orange',
+                calories: 85,
+                image: 'https://i.imgur.com/abKGOcv.jpg',
+                servings: 1,
+              }}
+            />
           </div>
         );
       })}
